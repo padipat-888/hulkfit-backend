3f1ad9342a4a4f91c50f6d9c12a15fda8dbf60bf
jizrix <jizrix@gmail.com>
2023-10-09 12:47:58 +0700

    connect to mongodb and route

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
-import express from "express";
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require("./models/User");
+
 const app = express();
-const port = 8000;
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://admin:mongodb@clusteruser.5pf96sm.mongodb.net/hulkfit?retryWrites=true&w=majority"
+
+mongoose
+  .connect(
+    uri,{ useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("connect success"))
+  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello my friends");
+  UserModel.find({})
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
 });
 
-app.get("/about-us", (req, res) => {
-  res.send("About Us");
+app.get("/getUserById/:id", (req, res) => {
+  const id = req.params.id;
+  UserModel.findById({ _id: id })
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
 });
 
-app.get("/admin", (req, res) => {
-  res.status(401).send("Admin only");
+app.post("/createUser", (req, res) => {
+  UserModel.create(req.body)
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
 });
 
-app.get("/home", (req, res) => {
-  res.redirect("/about-us");
+app.put("/updateUser/:id", (req, res) => {
+  const id = req.params.id;
+  UserModel.findByIdAndUpdate(
+    { _id: id },
+    { name: req.body.name, email: req.body.email, age: req.body.age })
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
 });
 
+app.delete(`/deleteUser/:id`, (req, res) => {
+    const id = req.params.id;
+    UserModel.deleteOne({ _id: id })
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
+})
+
+const port = 4000;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server Start in Port ${port}`);
 });
---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  fullname: String,
+  email: String,
+  pass: String,
+  img: String
+},{
+    collection:"user"
+});
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = UserModel;
