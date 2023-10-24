5f4262116ef096e45d821df10696de08d361eeed
padipat-888 <padipat123456789@gmail.com>
2023-10-24 12:55:40 +0700

    add api for create chart

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const UserModel = require('./models/User');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require('body-parser')
 const multer  = require('multer')
 const { v4: uuidv4 } = require('uuid');
 const bcrypt = require('bcrypt');
+const UserModel = require("./models/User");
 const ActivityModel = require('./models/Activity');
+const DashboardModel = require("./models/Dashboard");
 
 
 const storage = multer.diskStorage({
@@ -30,10 +31,9 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const uri ='mongodb+srv://admin:mongodb@clusteruser.5pf96sm.mongodb.net/hulkfit?retryWrites=true&w=majority';
 
-
-const uri =
-  'mongodb+srv://admin:mongodb@clusteruser.5pf96sm.mongodb.net/hulkfit?retryWrites=true&w=majority';
+//const uri = "mongodb+srv://padipat123456789:mongojsd@clusteruser.nl5he2u.mongodb.net/toeyhulkfit?retryWrites=true&w=majority"
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -43,10 +43,13 @@ app.get('/', (req, res) => {
     .then((user) => res.json(user))
     .catch((err) => res.json(err));
 });
+//note: get array of object of everything 
+//note: select * from users 
 
 app.get('/activitylist/', (req, res) => {
   console.log('Fetch Act Data');
   ActivityModel.find({})
+  .sort({ actDate: -1 }) // 1 for ascending order, -1 for descending
     .then((user) => res.json(user))
     .catch((err) => res.json(err));
 });
@@ -55,13 +58,39 @@ app.get('/activitylist/:id', (req, res) => {
   console.log('Fetch Act Data By Id');
   const userId = req.params.id
   ActivityModel.find({userId:userId})
+    .sort({ actType: 1 }) // 1 for ascending order, -1 for descending
     .then((user) => res.json(user))
     .catch((err) => res.json(err));
 });
 
+app.delete('/activitylist/delete/:id', (req, res) => {
+  console.log('User update');
+  const actId = req.params.id
+  console.log(actId)
+  ActivityModel.findByIdAndDelete(actId)
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
+});
+
+app.put('/activitylist/edit/:id', (req, res) => {
+  const actId = req.params.id
+  console.log(actId)
+  ActivityModel.findByIdAndUpdate(
+    {actId: actId},
+    {actName: req.body.actName, 
+     actType: req.body.actType, 
+     actDescription: req.body.actDescription,
+     actDuration: req.body.actDuration
+    }
+  )
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
+});
+
+
 app.put('/activitylist/update', async (req, res) => {
   console.log('User update');
-  
+
   const actId = req.body._id;
   const updatedData = req.body;
   console.log('request body is :',req.body)
@@ -75,11 +104,67 @@ app.put('/activitylist/update', async (req, res) => {
   }
 });
 
-app.delete('/activitylist/delete/:id', (req, res) => {
-  console.log('User Delete');
-  const actId = req.params.id
-  console.log(actId)
-  ActivityModel.findByIdAndDelete(actId)
+app.get('/sumofduration', async (req,res) => {
+  ActivityModel.aggregate([
+    {
+      $group: {
+        _id: null,
+        totalDuration: { $sum: "$actDuration" }
+      }
+    }
+  ]).then((user) => {
+    console.log(user)
+    res.json(user)
+  })
+  .catch((err) => res.json(err));
+})
+
+app.get('/sumofduration/:id', async (req,res) => {
+  console.log('Fetch Act Data By Id');
+  const userId = req.params.id
+  ActivityModel.aggregate([
+    {
+      $match: { userId: userId }
+    },
+    {
+      $group: {
+        _id: null,
+        totalDuration: { $sum: "$actDuration" }
+      }
+    }
+  ]).then((user) => {
+    console.log(user)
+    res.json(user)
+  })
+  .catch((err) => res.json(err));
+})
+
+app.get('/activitylist/dashboard/pie/:id', (req, res) => {
+  const userId = req.params.id
+  //DashboardModel.find({userId:userId})
+  ActivityModel.aggregate([
+    {
+      $match: { userId: userId } 
+    },
+    {
+      $group: { // aggregate must be $group
+        _id: '$actType', // first key of aggregate must be _id
+        totalDuration: { $sum: '$actDuration' }
+      }
+    },
+    {
+      $sort: { _id: 1 } // 1 for ascending order, -1 for descending
+    }
+  ])
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
+});
+
+app.get('/activitylist/dashboard/column/:id', (req, res) => {
+  console.log('Fetch Act Data By Id');
+  const userId = req.params.id
+  ActivityModel.find({userId:userId})
+    .sort({ actType: 1 }) // 1 for ascending order, -1 for descending
     .then((user) => res.json(user))
     .catch((err) => res.json(err));
 });
@@ -163,7 +248,6 @@ app.post('/addactivity',async (req, res) => {
   console.log(actDuration)
   console.log(actDate)
 
-
   const newAct = new ActivityModel({
     userId,
     actName,
@@ -181,74 +265,6 @@ app.post('/addactivity',async (req, res) => {
     res.status(500).json({ message: 'Error saving user', error });
   }
 });
-
-app.get('/sumofduration', async (req,res) => {
-  ActivityModel.aggregate([
-    {
-      $group: {
-        _id: null,
-        totalDuration: { $sum: "$actDuration" }
-      }
-    }
-  ]).then((user) => {
-    console.log(user)
-    res.json(user)
-  })
-  .catch((err) => res.json(err));
-})
-
-app.get('/sumofduration/:id', async (req,res) => {
-  console.log('Fetch Act Data By Id');
-  const userId = req.params.id
-  ActivityModel.aggregate([
-    {
-      $match: { userId: userId }
-    },
-    {
-      $group: {
-        _id: null,
-        totalDuration: { $sum: "$actDuration" }
-      }
-    }
-  ]).then((user) => {
-    console.log(user)
-    res.json(user)
-  })
-  .catch((err) => res.json(err));
-})
-
-app.get('/activitylist/dashboard/pie/:id', (req, res) => {
-  const userId = req.params.id
-  //DashboardModel.find({userId:userId})
-  DashboardModel.aggregate([
-    {
-      $match: { userId: userId } 
-    },
-    {
-      $group: { // aggregate must be $group
-        _id: '$actType', // first key of aggregate must be _id
-        totalDuration: { $sum: '$actDuration' }
-      }
-    },
-    {
-      $sort: { _id: 1 } // 1 for ascending order, -1 for descending
-    }
-  ])
-    .then((user) => res.json(user))
-    .catch((err) => res.json(err));
-});
-
-app.get('/activitylist/dashboard/column/:id', (req, res) => {
-    console.log('Fetch Act Data By Id');
-    const userId = req.params.id
-    DashboardModel.find({userId:userId})
-      .sort({ actType: 1 }) // 1 for ascending order, -1 for descending
-      .then((user) => res.json(user))
-      .catch((err) => res.json(err));
-  });
-
-
-
 
 
 const port = 4000;
